25dc04ba3b90dbb12f2283d0538509669ffa6e7d
Rezu3 <rezuphysics@gmail.com>
2026-08-24 19:49:40 +0530

    Update service worker cache configuration
    Updated cache name and added manifest.json to cache list.

--- static/sw.js
+++ static/sw.js
@@ -1,32 +1,15 @@
-// static/sw.js
-const CACHE_NAME = 'epsilon-v1';
+const CACHE_NAME = 'epsilon-v2';
 const urlsToCache = [
   '/',
   '/static/css/login.css',
-  '/static/css/admin_dashboard.css',
-  '/static/css/admin_students.css',
-  '/static/css/admin_teachers.css',
-  '/static/css/exam.css',
-  '/static/css/results.css',
   '/static/css/rank.css',
-  '/static/css/student_dashboard.css',
-  '/static/css/teacher_dashboard.css',
-  '/static/js/login.js',
-  '/static/js/admin_dashboard.js',
-  '/static/js/admin_students.js',
-  '/static/js/admin_teachers.js',
-  '/static/js/exam.js',
-  '/static/js/results.js',
-  '/static/js/rank.js',
-  '/static/js/student_dashboard.js',
-  '/static/js/teacher_home.js'
+  '/static/manifest.json'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
@@ -42,11 +25,11 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
